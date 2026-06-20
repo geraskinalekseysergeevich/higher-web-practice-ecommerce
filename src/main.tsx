add7d47bdfc5905fcc175df69da6bbf5d8894b88
geraskinalekseysergeevich <leha.geraskin.80@gmail.com>
2026-06-20 21:02:37 +0300

    feat: redux store foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
+import { StoreProvider } from './app/providers/StoreProvider';
 import './styles/tokens.css';
 import './styles/theme.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </StrictMode>,
 );
